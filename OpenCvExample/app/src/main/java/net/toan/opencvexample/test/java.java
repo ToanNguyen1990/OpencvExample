7e441ef2fe66bceac8bd0f0386ff72505e02c66b
toannv11 <toannv11@fsoft.com.vn>
2016-12-26 14:28:09 +0700

    [git common]

---
+++ OpenCvExample/app/src/main/java/net/toan/opencvexample/test/java.java
@@ -0,0 +1,12 @@
+package net.toan.opencvexample.test;
+
+/**
+ * Created by ToanNV11 on 12/26/2016.
+ */
+
+public class java {
+
+    public static void main(String[] args) {
+        System.out.print("ToanMaster");
+    }
+}
